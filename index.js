ff412c33c448e76d4f8f69b225d0dfdb6f236f5f
Mac kingsley <kingsleyudoma2018@gmail.com>
2025-10-15 08:55:02 +0100

    push

--- index.js
+++ index.js
@@ -18,10 +18,12 @@ mongoose.connect(process.env.MONGODB_URI, {})
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
-const authRoutes = require('../wwBackend/Routes/authRoutes');
+//const authRoutes = require('../wwBackend/Routes/authRoutes');
+const authRoutes = require("./Routes/authRoutes");
 const bomRoutes = require("../wwBackend/Routes/bomRoutes");
 const quotationRoutes = require("../wwBackend/Routes/quotationRoutes");
 const productRoutes = require("../wwBackend/Routes/productRoutes");
+
 app.use('/api/auth', authRoutes);
 app.use('/api/quotation', quotationRoutes);
 app.use('/api/bom', bomRoutes);
